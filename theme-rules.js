473dba7d6d9297761c1ffdf1fb115a0c5c2ae2ba
NonemJS <81621451+NonemJS@users.noreply.github.com>
2022-12-16 23:13:11 +0300

    Update theme-rules.js

--- theme-rules.js
+++ theme-rules.js
@@ -14,3 +14,19 @@ btn.addEventListener("click", function() {
     theme.href = "assets/css/rules-light.css";
   }
 });
+
+const btn2 = document.querySelector(".btn-toggle2");
+// Выбираем таблицу стилей
+const theme2 = document.querySelector("#theme-link");
+// Отслеживаем щелчок по кнопке
+btn2.addEventListener("click", function() {
+  // Если текущий адрес содержит "light-theme.css"
+  if (theme2.getAttribute("href") == "assets/css/rules-light.css") {
+    // …то переключаемся на "dark-theme.css"
+    theme2.href = "assets/css/rules.css";
+    // В противном случае… 
+  } else {
+    // …переключаемся на "light-theme.css"
+    theme2.href = "assets/css/rules-light.css";
+  }
+});
